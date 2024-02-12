530120a801f11bfcfe7d825ed910315d51d1880f
cooper <carringtonhate@gmail.com>
2024-02-11 21:36:01 -0800

    setting az zones for subnets

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -10,3 +10,10 @@ variable "private_subnet_cidrs" {
     description = "Private subnet cidr values"
     default = ["10.0.4.0/24","10.0.5.0/24","10.0.6.0/24"]
 }
+
+# az zones
+variable "azs"{
+    type = list(string)
+    description = "Availability zones"
+    default = ["us-east-1a","us-east-1b","us-east-1c"]
+}
