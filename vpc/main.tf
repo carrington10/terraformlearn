9ae3d4d610548f0d4dc27c65d16a5d624b625f24
cooper <carringtonhate@gmail.com>
2024-02-12 19:48:20 -0800

    connecting 2nd route table to public subnets

--- vpc/main.tf
+++ vpc/main.tf
@@ -44,5 +44,26 @@ resource "aws_internet_gateway" "gw"{
         Name = "internal gateway"
     }
 }
+# create second route table to connect public subnets to the internet
+resource "aws_route_table" "second_rt" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.gw.id
+    }
+
+        tags = {
+            Name = "2nd route table"
+        }
+}
+
+# associate public subnets with the second route table ( gives internet access)
+
+resource "aws_route_table_association" "public_subnet_asso" {
+    count = length(var.public_subnet_cidrs)
+    subnet_id = element(aws_subnet.public_subnets[*].id,count.index)
+    route_table_id = aws_route_table.second_rt.id
+}
 
  
